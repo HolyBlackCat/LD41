cb8af03a81c459927e89b36ef38c0e1d2a2df5db
HolyBlackCat <blckcat@inbox.ru>
2017-10-07 22:55:30 +0300

    Better window class. Still lacks exceptions though.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "mat.h"
-#include "movable_handle.h"
 #include "program.h"
 #include "ui.h"
 #include "window.h"
+#include "wrappers.h"
+#include "reflection.h"
+
+ReflectStruct(S, ((int)(a,b,c),))
 
 int main(int, char **)
 {
--- src/window.h
+++ src/window.h
@@ -5,65 +5,166 @@
 #include <utility>
 
 #include <SDL2/SDL.h>
+#include <GLFL/glfl.h>
 
 #include "mat.h"
-#include "movable_handle.h"
 #include "program.h"
+#include "reflection.h"
+#include "wrappers.h"
 
 class Window
 {
-    using WinHandleType = SDL_Window *;
-    struct WinHandleParam
+    struct WindowHandleFuncs
     {
-        std::string name;
-        ivec2 pos;
-        ivec2 size;
-        uint32_t flags;
+        static SDL_Window *Create(std::string name, ivec2 pos, ivec2 size, uint32_t flags)
+        {
+            return SDL_CreateWindow(name.c_str(), pos.x, pos.y, size.x, size.y, flags);
+        }
+        static void Destroy(SDL_Window *window)
+        {
+            SDL_DestroyWindow(window);
+        }
+        static void Error(...)
+        {
+            Program::Error("Can't create a window.");
+        }
     };
-    static WinHandleType WinHandleCreate(const WinHandleParam &param)
-    {
-        return SDL_CreateWindow(param.name.c_str(), param.pos.x, param.pos.y, param.size.x, param.size.y, param.flags);
-    }
-    static void WinHandleDestroy(WinHandleType handle)
-    {
-        SDL_DestroyWindow(handle);
-    }
-    static void WinHandleError(const WinHandleParam &)
-    {
-        Program::Error("Can't create a window.");
-    }
-    using WinHandle_t = MovableHandle<WinHandleType, WinHandleParam, WinHandleCreate, WinHandleDestroy, WinHandleError>;
 
-    using ConHandleType = SDL_GLContext;
-    using ConHandleParam = WinHandleType;
-    static ConHandleType ConHandleCreate(const ConHandleParam &window)
+    using WindowHandle = Wrappers::Handle<WindowHandleFuncs>;
+
+    struct ContextHandleFuncs
     {
-        return SDL_GL_CreateContext(window);
-    }
-    static void ConHandleDestroy(ConHandleType handle)
-    {
-        SDL_GL_DeleteContext(handle);
-    }
-    static void ConHandleError(const ConHandleParam &)
-    {
-        Program::Error("Can't create a GL context.");
-    }
-    using ConHandle_t = MovableHandle<ConHandleType, ConHandleParam, ConHandleCreate, ConHandleDestroy, ConHandleError>;
+        static SDL_GLContext Create(SDL_Window *window)
+        {
+            return SDL_GL_CreateContext(window);
+        }
+        static void Destroy(SDL_GLContext context)
+        {
+            SDL_GL_DeleteContext(context);
+        }
+        static void Error(...)
+        {
+            Program::Error("Can't create a GL context.");
+        }
+    };
+
+    using ContextHandle = Wrappers::Handle<ContextHandleFuncs>;
 
   public:
+    ReflectMemberEnum(SwapModes, (no_vsync)(vsync)(late_swap_tearing)(any_mode))
+    ReflectMemberEnum(Profiles, (core)(compatibility)(es)(any_profile))
+    ReflectMemberEnum(YesOrNo, (yes)(no)(dont_care))
+
     struct Settings
     {
+        ivec2 min_size = {};
         bool resizable = 0;
-        ivec2 pos = ivec2(SDL_WINDOWPOS_UNDEFINED);
-        bool fullscreen = 0;
-        bool keep_desktop_resolution_when_fullscreen = 0;
+
+        Reflect(Settings)
+        ( (bool)(fullscreen)(=0),
+          (bool)(keep_desktop_resolution_when_fullscreen)(=0),
+          (int)(display)(=0),
+          (bool)(make_centered)(=0),
+          (int)(gl_major)(=3),
+          (int)(gl_minor)(=3),
+          (Profiles)(gl_profile)(=compatibility),
+          (bool)(gl_debug)(=0),
+          (SwapModes)(swap_mode)(=late_swap_tearing),
+          (YesOrNo)(hardware_acceleration)(=dont_care),
+          (bool)(forward_compatibility)(=0),
+          (int)(msaa)(=0),
+          (ivec4)(color_bits)(={}),
+          (int)(depth_bits,stencil_bits)(=0), )
 
         Settings() {}
+        using ref = Settings &;
+        ref MinSize(ivec2 sz)
+        {
+            min_size = sz;
+            return *this;
+        }
+        ref Resizable(bool r)
+        {
+            resizable = r;
+            return *this;
+        }
+        ref FullScreen(bool f)
+        {
+            fullscreen = f;
+            return *this;
+        }
+        ref KeepDesktopResolutionWhenFullscreen(bool k)
+        {
+            keep_desktop_resolution_when_fullscreen = k;
+            return *this;
+        }
+        ref Display(int n)
+        {
+            display = n;
+            return *this;
+        }
+        ref MakeCentered(bool c)
+        {
+            make_centered = c;
+            return *this;
+        }
+        ref GlVersion(int maj, int min)
+        {
+            gl_major = maj;
+            gl_minor = min;
+            return *this;
+        }
+        ref GlProfile(Profiles p)
+        {
+            gl_profile = p;
+            return *this;
+        }
+        ref GlDebug(bool d)
+        {
+            gl_debug = d;
+            return *this;
+        }
+        ref SwapMode(SwapModes s)
+        {
+            swap_mode = s;
+            return *this;
+        }
+        ref HardwareAcceleration(YesOrNo s)
+        {
+            hardware_acceleration = s;
+            return *this;
+        }
+        ref ForwardCompatibility(YesOrNo f)
+        {
+            forward_compatibility = f;
+            return *this;
+        }
+        ref MSAA(int m)
+        {
+            msaa = m;
+            return *this;
+        }
+        ref ColorBits(ivec4 b)
+        {
+            color_bits = b;
+            return *this;
+        }
+        ref DepthBits(int b)
+        {
+            depth_bits = b;
+            return *this;
+        }
+        ref StencilBits(int b)
+        {
+            stencil_bits = b;
+            return *this;
+        }
     };
 
   private:
-    WinHandle_t window;
-    ConHandle_t context;
+    WindowHandle window;
+    ContextHandle context;
+    Wrappers::Ptr<glfl::context> func_context;
 
     std::string name;
     ivec2 size;
@@ -83,6 +184,8 @@ class Window
         settings = new_settings;
 
         uint32_t flags = SDL_WINDOW_OPENGL;
+        uint32_t context_flags = 0;
+
         if (settings.resizable)
             flags |= SDL_WINDOW_RESIZABLE;
         if (settings.fullscreen)
@@ -92,11 +195,103 @@ class Window
             else
                 flags |= SDL_WINDOW_FULLSCREEN;
         }
+        ivec2 pos;
+        if (settings.make_centered)
+            pos = ivec2(SDL_WINDOWPOS_CENTERED_DISPLAY(settings.display));
+        else
+            pos = ivec2(SDL_WINDOWPOS_UNDEFINED_DISPLAY(settings.display));
+        if (settings.gl_major || settings.gl_minor)
+        {
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, settings.gl_major);
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, settings.gl_minor);
+        }
+        switch (settings.gl_profile)
+        {
+          case core:
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+            break;
+          case compatibility:
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+            break;
+          case es:
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+            break;
+          default:
+            break;
+        }
+        if (settings.gl_debug)
+            context_flags |= SDL_GL_CONTEXT_DEBUG_FLAG;
+        switch (settings.hardware_acceleration)
+        {
+          case yes:
+            SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+            break;
+          case no:
+            SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 0);
+            break;
+          default:
+            break;
+        }
+        if (settings.forward_compatibility)
+            context_flags |= SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG;
+        if (settings.msaa == 1) settings.msaa = 0;
+        if (settings.msaa)
+        {
+            SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+            SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, settings.msaa);
+        }
+        if (settings.color_bits.r) SDL_GL_SetAttribute(SDL_GL_RED_SIZE    , settings.color_bits.r);
+        if (settings.color_bits.g) SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE  , settings.color_bits.g);
+        if (settings.color_bits.b) SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE   , settings.color_bits.b);
+        if (settings.color_bits.a) SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE  , settings.color_bits.a);
+        if (settings.depth_bits  ) SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE  , settings.depth_bits  );
+        if (settings.stencil_bits) SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, settings.stencil_bits);
 
-        WinHandle_t new_window  = {{name, settings.pos, size, flags}};
-        ConHandle_t new_context = {{*new_window}};
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, context_flags);
+
+
+        WindowHandle new_window = {{name, pos, size, flags}};
+        ContextHandle new_context = {{*new_window}};
         window  = new_window.move();
         context = new_context.move();
+
+        if (settings.min_size)
+            SDL_SetWindowMinimumSize(*window, settings.min_size.x, settings.min_size.y);
+
+        switch (settings.swap_mode)
+        {
+          case no_vsync:
+            if (SDL_GL_SetSwapInterval(0))
+            {
+                settings.swap_mode = vsync;
+                SDL_GL_SetSwapInterval(1);
+            }
+            break;
+          case vsync:
+            if (SDL_GL_SetSwapInterval(1))
+            {
+                settings.swap_mode = no_vsync;
+                SDL_GL_SetSwapInterval(0);
+            }
+            break;
+          case late_swap_tearing:
+            if (SDL_GL_SetSwapInterval(-1))
+            {
+                settings.swap_mode = vsync;
+                if (SDL_GL_SetSwapInterval(1))
+                {
+                    settings.swap_mode = no_vsync;
+                    SDL_GL_SetSwapInterval(0);
+                }
+            }
+            break;
+          default:
+            break;
+        }
+
+        func_context.alloc();
+        glfl::active_context(func_context);
+
     }
 
     void Destroy()
@@ -108,6 +303,11 @@ class Window
     bool Exists() const
     {
         return bool(window);
+    }
+
+    const Settings &GetSettings() const
+    {
+        return settings;
     }
 };
 
